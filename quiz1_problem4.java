a025362418e1d82bddfce37a9f99b44242647738
AregSahakyan04 <79696542+AregSahakyan04@users.noreply.github.com>
2025-02-09 22:17:06 +0400

    Create quiz1_problem4.java

---
+++ quiz1_problem4.java
@@ -0,0 +1,19 @@
+// Write a program that displays the following table
+// a  a^2 a^3
+// 1   1   1
+// 2   4   8
+// 3   9  27
+// 4  16  64
+
+
+public class main{
+    public static void main (String[] args) {
+        int a = 1;
+        System.out.print("a   a^2 a^3 \n");
+        
+        for (int i = 0; i < 4; i++){
+            System.out.println(a + "   " + (a * a) + "   " + (a * a * a));
+            a++;
+        }
+    }
+}
