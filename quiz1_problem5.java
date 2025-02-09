d27641db970d10b1c9dc6141335a3bbf0769faaa
AregSahakyan04 <79696542+AregSahakyan04@users.noreply.github.com>
2025-02-09 22:23:37 +0400

    Update quiz1_problem5.java

--- quiz1_problem5.java
+++ quiz1_problem5.java
@@ -1,1 +1,9 @@
+// Write a program that displays the result of
+//      9.5*4.5-2.5*3 / 45.5-3.5
 
+public class main {
+    public static void main(String[] args) {
+        double result = (9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5);
+        System.out.println("Result: " + result);
+    }
+}
