d4952ae09a855dcc3fe1ba433fe78f31d756be59
AregSahakyan04 <79696542+AregSahakyan04@users.noreply.github.com>
2025-02-10 13:16:36 +0400

    Update quiz1_problem9.java

--- quiz1_problem9.java
+++ quiz1_problem9.java
@@ -1,1 +1,11 @@
+public class main {
+    public static void main(String[] args) {
 
+        double height = 7.9;
+        double width = 4.5;
+
+        double area = height * width;
+        
+        System.out.println(area);
+    }
+}
