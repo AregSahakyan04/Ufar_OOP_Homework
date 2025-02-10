1f525b0d1b7d7ce7639420779fae40043c4a8e83
AregSahakyan04 <79696542+AregSahakyan04@users.noreply.github.com>
2025-02-11 00:10:27 +0400

    Update Q2P06.java

--- Q2P06.java
+++ Q2P06.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
 
-public class Q2P6 {
+public class Q2P06 {
     public static void main(String[] args) {
         System.out.print("Enter an integer: ");
         Scanner input = new Scanner(System.in);
         
-        int num = input.nextInt();
-        System.out.println(DigSum(num));
+        int Num = input.nextInt();
+        System.out.println(DigSum(Num));
     }
   
     private static int DigSum(int i) {
-        int sum = 0;
+        int Sum = 0;
         while (i > 0) {
-            sum += (i % 10);
+            Sum += (i % 10);
             i /= 10;
         }
-        return sum;
+        return Sum;
     }
 }
