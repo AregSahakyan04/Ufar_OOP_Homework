d12bae1222456b3e3d052bfba4370b0f7910774e
AregSahakyan04 <79696542+AregSahakyan04@users.noreply.github.com>
2025-02-10 13:11:10 +0400

    Update quiz1_problem10.java

--- quiz1_problem10.java
+++ quiz1_problem10.java
@@ -1,1 +1,17 @@
+public class main {
+    public static void main(String[] args) {
 
+        double distanceKilometers = 14.0;
+        double timeMinutes = 45.0;
+        double timeSeconds = 30.0;
+        double kilometersToMiles = 1.6;
+
+        double distanceMiles = distanceKilometers / kilometersToMiles;
+
+        double timeHours = (timeMinutes + timeSeconds / 60.0) / 60.0;
+
+        double averageSpeed = distanceMiles / timeHours;
+
+        System.out.println(averageSpeed);
+    }
+}
