9b318bad1704da77ec029acd31d5ffca1579b84f
AregSahakyan04 <79696542+AregSahakyan04@users.noreply.github.com>
2025-02-10 13:02:51 +0400

    Update quiz1_problem6.java

--- quiz1_problem6.java
+++ quiz1_problem6.java
@@ -1,1 +1,13 @@
-
+public class Main {
+    public static void main(String[] args) {
+        // First approximation
+        double pi1 = 4.0 * (1.0 - 1.0 / 3 + 1.0 / 5 - 1.0 / 7 + 1.0 / 9 - 1.0 / 11);
+        
+        // Second approximation
+        double pi2 = 4.0 * (1.0 - 1.0 / 3 + 1.0 / 5 - 1.0 / 7 + 1.0 / 9 - 1.0 / 11 + 1.0 / 13);
+        
+        // Print results
+        System.out.println("First approximation of π: " + pi1);
+        System.out.println("Second approximation of π: " + pi2);
+    }
+}
