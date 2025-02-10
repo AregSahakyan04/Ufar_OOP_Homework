90029b2f6caa924fc95cd736f37197e9d5f0f9d5
AregSahakyan04 <79696542+AregSahakyan04@users.noreply.github.com>
2025-02-10 23:21:56 +0400

    Create Q2P4.java

---
+++ Q2P4.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Q2P4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a value of pounds: ");
+        double lb = input.nextDouble();
+        double kg = lb * 0.454;
+        System.out.println(lb + " pounds is equal to " + kg + " kilograms");
+    }
+}
