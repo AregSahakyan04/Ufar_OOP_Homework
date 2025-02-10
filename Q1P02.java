ee2e283fc7a4a34f8fb71fea7187de4c63f433ab
AregSahakyan04 <79696542+AregSahakyan04@users.noreply.github.com>
2025-02-11 00:00:07 +0400

    Update Q1P02.java

--- Q1P02.java
+++ Q1P02.java
@@ -1,6 +1,6 @@
 // Write a program that displays Welcome to Java five times.
 
-public class Q1P2 {
+public class Q1P02 {
     public static void main(String[] args) {
         for (int i = 0; i < 5 ; i++ ) {
             System.out.print("Welcome to Java \n");
