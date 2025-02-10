3f81e947cc62e611b95d64524b16490ed2f3a880
AregSahakyan04 <79696542+AregSahakyan04@users.noreply.github.com>
2025-02-10 23:35:07 +0400

    Update Q2P1.java

--- Q2P1.java
+++ Q2P1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Q2P2 {
+public class Q2P1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the celsius: ");
