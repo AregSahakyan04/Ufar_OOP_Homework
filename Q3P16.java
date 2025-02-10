f372f5040abbef7b57d7d7d3389a8d527ddbc426
AregSahakyan04 <79696542+AregSahakyan04@users.noreply.github.com>
2025-02-11 01:18:45 +0400

    Create Q3P16.java

---
+++ Q3P16.java
@@ -0,0 +1,24 @@
+
+import java.util.Random;
+
+public class Q3P16 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        
+        boolean negOrPosX = random.nextBoolean();
+        boolean negOrPosY = random.nextBoolean();
+        
+        int x = (int) ((Math.random() * 100));
+        int y = (int) ((Math.random() * 200));
+        
+        if (negOrPosX) {
+            x = x * -1;
+        }
+        
+        if (negOrPosY) {
+            y = y * -1;
+        }
+        
+        System.out.println("(" + x + "," + y + ")");
+    }
+}
