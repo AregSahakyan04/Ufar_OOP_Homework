b2c9c7dcbd1bde074d2d837722b494333c1716fc
AregSahakyan04 <79696542+AregSahakyan04@users.noreply.github.com>
2025-02-11 00:06:36 +0400

    Update Q2P04.java

--- Q2P04.java
+++ Q2P04.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Q2P4 {
+public class Q2P04 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a value of pounds: ");
