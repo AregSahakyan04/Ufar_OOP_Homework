cd22bb1dbf7dd0845e2a6443a5d763df32ac2ac9
AregSahakyan04 <79696542+AregSahakyan04@users.noreply.github.com>
2025-02-10 23:32:19 +0400

    Update Q1P7.java

--- Q1P7.java
+++ Q1P7.java
@@ -1,4 +1,4 @@
-public class Main {
+public class Q1P7 {
     public static void main(String[] args) {
         // First approximation
         double pi1 = 4.0 * (1.0 - 1.0 / 3 + 1.0 / 5 - 1.0 / 7 + 1.0 / 9 - 1.0 / 11);
