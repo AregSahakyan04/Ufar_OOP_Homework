e330d5e996e573d3339bf68d17df61b9a681457a
AregSahakyan04 <79696542+AregSahakyan04@users.noreply.github.com>
2025-02-10 23:31:19 +0400

    Update and rename quiz1_problem5.java to Q1P5.java

--- Q1P5.java
+++ Q1P5.java
@@ -1,7 +1,7 @@
 // Write a program that displays the result of
 //      9.5*4.5-2.5*3 / 45.5-3.5
 
-public class main {
+public class Q1P5 {
     public static void main(String[] args) {
         double result = (9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5);
         System.out.println("Result: " + result);
