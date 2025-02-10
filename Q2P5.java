4f252f8f5bc4799572650164c66ec3c3a698d00e
AregSahakyan04 <79696542+AregSahakyan04@users.noreply.github.com>
2025-02-10 23:37:53 +0400

    Update Q2P5.java

--- Q2P5.java
+++ Q2P5.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class C2_5 {
+public class Q2P5 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter subtotal and the gratuity rate: ");
