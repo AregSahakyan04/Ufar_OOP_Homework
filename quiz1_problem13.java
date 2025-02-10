678b88fc7146caf95fb6aae6ca154e579674f7f8
AregSahakyan04 <79696542+AregSahakyan04@users.noreply.github.com>
2025-02-10 13:27:16 +0400

    Update quiz1_problem13.java

--- quiz1_problem13.java
+++ quiz1_problem13.java
@@ -1,1 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
 
+        double a = 3.4;
+        double b = 50.2;
+        double e = 44.5;
+        double c = 2.1;
+        double d = 0.55;
+        double f = 5.9;
+
+        double denominator = (a * d) - (b * c);
+
+        if (denominator != 0) {
+            
+            double x = (e * d - b * f) / denominator;
+            double y = (a * f - e * c) / denominator;
+
+            System.out.printf("The value of x is: " + x);
+            System.out.printf("The value of y is: " + y);
+        } 
+        else {
+            System.out.println("The equations have no unique solution.");
+        }
+    }
+}
