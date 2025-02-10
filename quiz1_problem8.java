0609a55a2d1e2c330e824f1609c2f9129c2ce43c
AregSahakyan04 <79696542+AregSahakyan04@users.noreply.github.com>
2025-02-10 13:14:23 +0400

    Update quiz1_problem8.java

--- quiz1_problem8.java
+++ quiz1_problem8.java
@@ -1,1 +1,14 @@
+public class main {
+    public static void main(String[] args) {
 
+        double radius = 5.5;
+        double pi = Math.PI;
+
+        double perimeter = 2 * radius * pi;
+
+        double area = radius * radius * pi;
+
+        System.out.println(perimeter);
+        System.out.println(area);
+    }
+}
