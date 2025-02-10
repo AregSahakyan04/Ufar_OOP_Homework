12e4c65233a1f985d878ddf278a40137f0505e15
AregSahakyan04 <79696542+AregSahakyan04@users.noreply.github.com>
2025-02-10 23:27:03 +0400

    Update Q1P1.java

--- Q1P1.java
+++ Q1P1.java
@@ -1,7 +1,7 @@
 // Write a program that displays Welcome to Java, Welcome to Computer Science, and Programming is fun.
 
 
-public class main {
+public class Q1P1 {
     public static void main(String[] args) {
         System.out.print("Welcome to Java \n");
         System.out.print("Welcome to Computer \n");
